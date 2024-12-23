a498689b9f6b42ac81175bbbf15bb748371dce48
TrueX <truexrevs@gmail.com>
2024-12-22 22:04:21 -0500

    added keys

---
+++ pydantic_models.py
@@ -0,0 +1,33 @@
+
+'''
+Pydantic is a data validation library that uses Python type annotations to define data schemas. 
+In our FastAPI application, we use Pydantic models to define the structure of our request and response data. 
+Let's break down the models defined in
+'''
+
+from pydantic import BaseModel, Field
+from enum import Enum
+from datetime import datetime
+
+class ModelName(str, Enum):
+    GPT4_O = "gpt-4o"
+    GPT4_O_MINI = "gpt-4o-mini"
+
+class QueryInput(BaseModel):
+    question: str
+    session_id: str = Field(default=None)
+    model: ModelName = Field(default=ModelName.GPT4_O_MINI)
+
+class QueryResponse(BaseModel):
+    answer: str
+    session_id: str
+    model: ModelName
+
+class DocumentInfo(BaseModel):
+    id: int
+    filename: str
+    upload_timestamp: datetime
+
+class DeleteFileRequest(BaseModel):
+    file_id: int
+
