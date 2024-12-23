ed395469456f1853b864a816a31e303f1701068f
TrueX <truexrevs@gmail.com>
2024-12-22 20:06:00 -0500

    first commit

---
+++ intro.py
@@ -0,0 +1,20 @@
+
+# python -m venv myfastapi
+# myfastapi\Scripts\Activate.ps1
+# python.exe -m pip install --upgrade pip
+# pip3 install -r requirements.txt
+# pip install ipykernel 
+
+
+'''
+# project structure
+rag-with-fastapi/
+│
+├── main.py
+├── chroma_utils.py
+├── db_utils.py
+├── langchain_utils.py
+├── pydantic_models.py
+├── requirements.txt
+└── chroma_db/  (directory for Chroma persistence)
+'''
